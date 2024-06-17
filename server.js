f9425361312c447bb78a12dd679870cb7e20ad32
felixd38v <felixd38v@icloud.com>
2024-06-17 12:53:28 -0500

    add array

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ app.use(express.urlencoded({ extended:true}));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
+app.get('/users', (req, res) => {
+    const users = [
+        { id: 1, name: 'María Rodríguez' },
+        { id: 2, name: 'Mariela Pérez' },
+        { id: 3, name: 'Alberto Zurita'},
+        ] 
+        res.send(` 
+            <h2>Users</h2>
+            <ul class="list-group">
+                ${users.map((user)=>`<li class="list-group-item">${user.name}</li>`).join('')} 
+    </ul>
+    `) });
+
 // Start the server
 app.listen(3000, ()=>{
     console.log('Server listening on port 3000')
