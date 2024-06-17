3c34f4bd160d934d179fba355d9a08978ba496c6
felixd38v <felixd38v@icloud.com>
2024-06-17 14:18:26 -0500

    add spinner

--- server.js
+++ server.js
@@ -11,18 +11,28 @@ app.use(express.urlencoded({ extended:true}));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.get('/users', (req, res) => {
-    const users = [
+app.get('/users', async (req, res) => {
+    setTimeout(async()=> {
+
+
+    /*const users = [
         { id: 1, name: 'María Rodríguez' },
         { id: 2, name: 'Mariela Pérez' },
         { id: 3, name: 'Alberto Zurita'},
-        ] 
+        ] */
+        const limit =+req.query.limit || 7
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users?_limit=${limit}`)
+        const users = await response.json()
+
+
         res.send(` 
             <h2>Users</h2>
             <ul class="list-group">
                 ${users.map((user)=>`<li class="list-group-item">${user.name}</li>`).join('')} 
     </ul>
-    `) });
+    `)
+}, 2000)
+ });
 
 // Start the server
 app.listen(3000, ()=>{
